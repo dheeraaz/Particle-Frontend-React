837a3d0afc308430f7b6e916d98711fdc13060f1
Dhiraj Acharya <076bct023.dhiraj@pcampus.edu.np>
2024-06-24 06:48:25 +0545

    configured tailwind css for larger screen first

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '2xl': {'max': '1536px'},
+        // => @media (max-width: 1536px) { ... }
+  
+        'xl': {'max': '1280px'},
+        // => @media (max-width: 1280px) { ... }
+  
+        'lg': {'max': '1024px'},
+        // => @media (max-width: 1024px) { ... }
+  
+        'md': {'max': '768px'},
+        // => @media (max-width: 768px) { ... }
+  
+        'sm': {'max': '640px'},
+        // => @media (max-width: 640px) { ... }
+      },
+      fontFamily:{
+        'poppins': ['Poppins', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
