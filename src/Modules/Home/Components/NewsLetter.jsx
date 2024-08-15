cdd123b561b0833d2de58061d0c72473181fdd19
dhirajacharya <076bct023.dhiraj@pcampus.edu.np>
2024-08-15 10:28:45 +0545

    changed blogs count in home page

--- src/Modules/Home/Components/NewsLetter.jsx
+++ src/Modules/Home/Components/NewsLetter.jsx
@@ -7,7 +7,7 @@ const NewsLetter = () => {
             <div className='px-[353px] py-16 xl:px-[317px] xl:py-[57px] text-center'>
                 <h3 className='subHeading'>{capitalizeWords('subscribe to our newsletter')}</h3>
                 <p className=' font-links font-medium text-xs xl:text-[10px] text-extraLightText'>We’ll keep you in the loop on our best and latest blog posts.</p>
-                <div className='mt-12 h-14 xl:mt-11 xl:h-[50px] w-full flex items-center justify-between gap-10 xl:gap-9'>
+                <form className='mt-12 h-14 xl:mt-11 xl:h-[50px] w-full flex items-center justify-between gap-10 xl:gap-9'>
                     <input
                         type="text"
                         id="simple-search"
@@ -17,7 +17,7 @@ const NewsLetter = () => {
                     />
                     <button className='searchShadow h-full py-[13px] px-12 text-xl xl:py-2 xl:px-11 xl:text-base rounded-full bg-accent font-links   text-bodyColor font-normal'>Subscribe</button>
 
-                </div>
+                </form>
             </div>
         </section>
     )
--- src/Modules/Home/Components/RecentBlogs.jsx
+++ src/Modules/Home/Components/RecentBlogs.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BlogCard } from '../../../Common/Components'
 import { blogContent } from '../../../Constants/contents/blogContents'
 
 const RecentBlogs = () => {
+    const [isXl, setIsXl] = useState(document.documentElement.clientWidth<=1280)
+    useEffect(() => {
+        function getWindowWidth() {
+            // Get width of the window excluding scrollbars
+            let w = document.documentElement.clientWidth;
+            setIsXl(w<=1280);
+        }
+
+        // Attaching the event listener function to window's resize event
+        window.addEventListener("resize", getWindowWidth);
+
+        // removing eventlistener on component unmount
+        return ()=>{
+            window.removeEventListener("resize", getWindowWidth)
+        }
+    }, [])
+
+    const blogsToShow = isXl?3:4;
+
     return (
         <section className='mt-14 mb-20 xl:mt-10 xl:mb-16 container'>
             <h3 className='subHeading mb-9 xl:mb-8'><span className='text-accent'>Recent</span> Blogs</h3>
             {/* recent blogs section */}
             <div className='w-full grid grid-cols-4 gap-10 xl:grid-cols-3 xl:gap-8'>
-                {blogContent[0].chapters.slice(0, 4).map((blog) => {
+                {blogContent[0].chapters.slice(0, blogsToShow).map((blog) => {
                     return <BlogCard
                         key={blog.id}
                         title={blog.title}
