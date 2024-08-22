8d93038bd55701246a215b131a4b9acb72169662
dhirajacharya <076bct023.dhiraj@pcampus.edu.np>
2024-08-22 14:08:20 +0545

    responsive design of chapter page

--- src/Common/Components/SubjectDescription.jsx
+++ src/Common/Components/SubjectDescription.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const SubjectDescription = (props) => {
     const {imgSrc, subjectTitle,  description=[] } = props
   return (
-    <section className='my-6 flex gap-16 xl:my-5 xl:gap-14 justify-between items-center'>
-    <div className='w-[547px] h-[278px] xl:w-[496px] xl:h-[252px] rounded-lg'>
+    <section className='my-6 flex gap-16 xl:my-5 xl:gap-14 justify-between items-center md:flex-col md:gap-4'>
+    <div className='w-[547px] h-[278px] xl:w-[496px] xl:h-[252px] lg:w-[360px] lg:h-[220px] md:w-full md:h-full rounded-lg'>
       <img src={imgSrc} alt={`${subjectTitle}_img`} className='w-full h-full rounded-lg' />
     </div>
     <div className='flex flex-1 flex-col gap-8 text-justify text-base font-light xl:text-sm xl:gap-7'>
--- src/Common/pages/ChaptersPage.jsx
+++ src/Common/pages/ChaptersPage.jsx
@@ -61,8 +61,8 @@ const BlogsChaptersPage = () => {
       <SubjectDescription imgSrc={singleSubject?.subjectPageImage} subjectTitle={singleSubject?.subjectTitle} description={singleSubject?.subjectPageIntro} />
 
       {/* Bottom Section */}
-      <section className='my-20 xl:my-[72px] flex justify-between'>
-        <aside className='w-[733px] xl:w-[664px] pr-[60px] border-r-[1px] border-extraLightText'>
+      <section className='my-20 md:my-2 xl:my-[72px] flex justify-between lg:flex-col lg:gap-6'>
+        <aside className='w-[733px] xl:w-[664px] pr-[60px] border-r-[1px] border-extraLightText lg:w-full lg:border-none lg:p-0'>
           <h3 className=' text-primary text-[32px] xl:text-[30px] font-medium'>{capitalizeWords(contentType)} / Contents</h3>
           <div className='mt-8 xl:mt-7'>
             {singleSubject?.chapters?.map((chap, index) => {
@@ -72,14 +72,15 @@ const BlogsChaptersPage = () => {
             })}
           </div>
         </aside>
-        <aside className='w-[547px] xl:w-[496px] ml-10 xl:ml-9'>
-          <h3 className='text-2xl xl:text-xl font-medium text-primary'>You May Also Like</h3>
-          <div className='mt-[31px] xl:mt-[27px] flex flex-col gap-9'>
-            {releventItems.slice(0, 3).map((item, index) => {
-              return <ReleventCard key={index} title={item.chapterTitle} imageSrc={item.imgSrc} date={item.createdAt} cardRoute={`/${contentType}/${item.subjectTitle}/${item.chapterId}`} />
-            })}
+        <aside className='w-[547px] xl:w-[496px] ml-10 xl:ml-9 lg:ml-0 lg:w-full'>
+          <h3 className='text-2xl xl:text-xl lg:text-2xl font-medium text-primary'>You May Also Like</h3>
+          <div className='lg:flex lg:justify-center'>
+            <div className='mt-[31px] xl:mt-[27px] flex flex-col gap-9 lg:grid lg:grid-cols-3 lg:gap-10 md:grid-cols-2 sm:grid-cols-1'>
+              {releventItems.slice(0, 3).map((item, index) => {
+                return <ReleventCard key={index} title={item.chapterTitle} imageSrc={item.imgSrc} date={item.createdAt} cardRoute={`/${contentType}/${item.subjectTitle}/${item.chapterId}`} />
+              })}
+            </div>
           </div>
-
         </aside>
       </section>
     </div>
