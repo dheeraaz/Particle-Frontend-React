ec020d520e900e9bc47f2649dc48ac0f3faa1083
dhirajacharya <076bct023.dhiraj@pcampus.edu.np>
2024-10-31 10:46:19 +0545

    added framer motion

--- src/Common/Components/Header.jsx
+++ src/Common/Components/Header.jsx
@@ -1,36 +1,41 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Logo, Navbar, RightNav } from '../Utility/index.js'
+
+import { motion, useScroll, useMotionValueEvent } from "framer-motion"
 
 
 
 const Header = () => {
-  const [isScroll, setIsScroll] = useState(false);
   const [toggleMenu, setToggleMenu] = useState(true);
+  const [isHidden, setIsHidden] = useState(false);
 
-  function handleScroll() {
-    if(window.scrollY > 320){
-      setIsScroll(true);
+  const { scrollY } = useScroll();
+
+  useMotionValueEvent(scrollY, "change", (currScrollPosn) => {
+    const prevScrollPosn = scrollY.getPrevious();
+    if(currScrollPosn > prevScrollPosn && currScrollPosn > 320){
+      setIsHidden(true)
     }else{
-      setIsScroll(false)
+      setIsHidden(false)
     }
-  }
-  useEffect(() => {
-    //this will add scroll eventlistener and its corresponding function to window
-    window.addEventListener('scroll', handleScroll);
-    //this will remove scroll eventlistener when a component is removed
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [])
+  })
 
   return (
-    <header className={`flex items-center bg-primary transition-all ease-in-out h-24 xl:h-20 z-10 ${isScroll?('sticky top-0 shadow-lg'):('')}`}>
+    <motion.header
+      variants={{
+        visible: { y: 0 },
+        hidden: { y: "-100%" },
+      }}
+      animate={isHidden ? "hidden" : "visible"}
+      transition={{ duration: 0.3, ease:"easeInOut"}}
+
+      className="flex items-center bg-primary transition-all ease-in-out h-24 xl:h-20 z-10 sticky top-0">
       <div className='container flexVerCenter justify-between'>
         <Logo />
-        <Navbar toggleMenu = {toggleMenu}/>
-        <RightNav toggleMenu = {toggleMenu} setToggleMenu={setToggleMenu}/>
+        <Navbar toggleMenu={toggleMenu} />
+        <RightNav toggleMenu={toggleMenu} setToggleMenu={setToggleMenu} />
       </div>
-    </header>
+    </motion.header>
   )
 }
 
--- src/Common/Utility/Navbar.jsx
+++ src/Common/Utility/Navbar.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { navLinks } from '../../Constants/navLinks.js'
 import { NavLink } from 'react-router-dom'
+
+import { motion } from "framer-motion"
+import { MdDisabledVisible } from 'react-icons/md'
+
 
 const MyNavlinksCallback = ({ isActive }) => {
   return ` hover:text-accent ${isActive ? "text-accent" : "text-bodyColor"}`
 }
 
-const Navbar = ({toggleMenu}) => {
+const Navbar = ({ toggleMenu }) => {
+  const [isUptoMedium, setIsUptoMedium] = useState(false);
+
+  useEffect(() => {
+    function determineScreenSize() {
+      let w = document.documentElement.clientWidth;
+      setIsUptoMedium(w <= 768);
+    }
+
+    determineScreenSize();
+    window.addEventListener("resize", determineScreenSize);
+
+    return () => {
+      window.removeEventListener("resize", determineScreenSize);
+    }
+  }, []);
+
   return (
-    <nav 
+    <motion.nav 
+    variants={isUptoMedium ? {
+      visible: { opacity:1,x: 0, },
+      hidden: { opacity:0,x: 200 },
+    } : {}}
+    animate={isUptoMedium && toggleMenu ? "hidden" : "visible"}
+    transition={isUptoMedium ? { duration: 0.5, ease:"backOut" } : {}}
+
     className={`font-links flex gap-5 xl:gap-4 lg:gap-3 font-medium text-base xl:text-sm lg:text-xs ${toggleMenu?"md:hidden":"md:block md:py-10 md:z-50 md:absolute md:top-[60px] md:right-0 md:w-1/2 md:h-fit md:bg-primary"}`}>
       {/* <div className='font-links flex gap-5 xl:gap-4 lg:gap-3 font-medium text-base xl:text-sm lg:text-xs md:hidden'> */}
       <div className={`font-links flex gap-5 xl:gap-4 lg:gap-3 font-medium text-base xl:text-sm lg:text-xs ${toggleMenu?"":"md:flex-col md:justify-center md:items-center md:gap-10 md:text-base"}`}>
@@ -20,7 +47,7 @@ const Navbar = ({toggleMenu}) => {
           })
         }
       </div>
-    </nav>
+    </motion.nav>
   )
 }
 
